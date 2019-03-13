fce72647dd471d9388cca8443c7b67805c1a84e1
Ritalffy <plesinska.marta@gmail.com>
2019-03-13 16:24:37 +0100

    Funckja_Policz_Master

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ritalffy
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Wynik 2+3="+Policz(2,3));
+        // TODO code application logic here
+    }
+    public static int Policz (int a, int b) 
+    {
+        return a+b;
+    }
+}
